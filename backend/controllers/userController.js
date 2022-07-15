07d45ee51d6419ac20fb01a481624c0d823fa7bf
Hamza El Meknassi <elmeknassi.h@gmail.com>
2022-07-15 11:59:07 +0200

    Implement login with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,26 @@
 const { insertUser, getUser } = require("../services/mongoService");
 
 async function login(req, res, next) {
+  if (!validateUserBodyOrFail(req, res)) return;
+  let result;
+  try {
+    // Get user by name
+    const storedUser = await getUser(req.body.name);
+    if (!storedUser) return res.status(404).send({ error: "User does not exist" })
 
+    // Creating a HMAC digest of the user password to compare with stored one
+    const hmac = require('crypto').createHmac('sha256', process.env.HMAC_SECRET);
+    hmac.update(req.body.password);
+    if (storedUser.password === hmac.digest("hex")) return res.status(200).send({ jwt: generateAccessToken(storedUser.name) });
+    else return res.status(401).send({ error: "Wrong username or password" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error });
+  }
 }
 
 async function register(req, res, next) {
-  if (!req.body.name || !req.body.password || req.body.password.length < 3) res.status(400).send({
-    error: "Expected body with 'name' string(1+) and 'password' string(3+)"
-  });
+  if (!validateUserBodyOrFail(req, res)) return;
   let result;
   try {
     // Check if user exists
@@ -23,6 +36,20 @@ async function register(req, res, next) {
     console.error(error);
     res.status(500).send({ error });
   }
+}
+
+function validateUserBodyOrFail(req, res) {
+  if (!req.body.name || !req.body.password || req.body.password.length < 3) {
+    res.status(400).send({
+      error: "Expected body with 'name' string(1+) and 'password' string(3+)"
+    });
+    return false;
+  }
+  return true;
+}
+
+function generateAccessToken(name) {
+  return require('jsonwebtoken').sign({ user: name }, process.env.JWT_SECRET, { expiresIn: 1800 });
 }
 
 module.exports = { login, register };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { register, login } = require('../controllers/userController');
 var router = express.Router();
 
+router.post('/', login);
 router.put('/', register);
 
 module.exports = router;
