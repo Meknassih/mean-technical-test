bcb8ba4176d6f673f20cec4ffbec219cd804c77d
Hamza El Meknassi <elmeknassi.h@gmail.com>
2022-07-17 20:57:49 +0200

    Scaffold views : login & home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { LoginComponent } from './view/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    title: "Login — MTL",
+    component: LoginComponent
+  },
+  {
+    path: "",
+    title: "Home — MTL",
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginComponent } from './view/login/login.component';
+import { HomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
